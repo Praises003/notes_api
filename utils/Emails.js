386f05a171a9c2b2958df419ca64400740fd8e55
Praises003 <praiseabiodun15@gmail.com>
2025-07-01 00:46:31 +0100

    update

--- utils/Emails.js
+++ utils/Emails.js
@@ -4,8 +4,8 @@ const sendMail = async (to, subject, text) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
         }
     });
 
